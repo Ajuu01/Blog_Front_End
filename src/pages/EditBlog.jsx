9ff5cf7ffb761de81270abe659d910356bd6a021
Ajuu01 <raieajuna1005@gmail.com>
2026-02-18 22:20:45 +0545

    Added editBlog

---
+++ src/pages/EditBlog.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+function EditBlog(){
+    const {id}=useParams()
+    const navigate=useNavigate()
+    
+    const [data,setData]=useState({
+        title:"",
+        subtitle:"",
+        image:"",
+        description:""
+    })
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target
+        setData({
+            ...data,
+            [name]:name==="image"?e.target.files[0]:value
+        })
+    }
+    useEffect(() => {
+        const fillBlog=async()=>{
+                const response=await axios.get("http://localhost:3000/blog/"+id)
+                const blog=response.data.data
+
+                setData({
+                    title:blog.title,
+                    subtitle:blog.subtitle,
+                    image:blog.image,
+                    description:blog.description
+                })
+        }
+        fillBlog()
+    },[id])
+    const editBlog=async(e)=>{
+            e.preventDefault()
+            
+            const response=await axios.patch("http://localhost:3000/blog/"+id,data,{
+                headers:{
+                    "Content-Type":"multipart/form-data"
+                }
+            })
+            if(response.status==200){
+                navigate("/")            
+            }
+            else{
+                alert("Something Went Wrong!")
+            }
+        }
+    return (
+        <>
+            <Navbar/>
+            <div className="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+                <div className="mt-10 text-center font-bold">Wanna Edit a Blog?</div>
+                <div className="mt-3 text-center text-4xl font-bold">Edit a Blog</div>
+                <form onSubmit={editBlog}>
+                <div className="p-8">
+                    <div className="flex gap-4">
+                        <input type="text" name="title" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}/>
+                        <input type="text" name="subtitle" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}/>
+                    </div>
+                    <div className="my-6 flex gap-4">
+                        <img src={`http://localhost:3000/${data.image}`} width="200" alt="Current"/>
+                        <input type="file" name="image" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange}/>
+                    </div>
+                    <div className="">
+                        <textarea name="description" id="text" cols="30" rows="10" className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"  placeholder="Description *" onChange={handleChange} value={data.description}></textarea>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default EditBlog
--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function SingleBlog(){
     const {id}=useParams()
@@ -40,7 +40,9 @@ function SingleBlog(){
                         </div>
                         <div className="flex -mx-2 mb-4">
                             <div className="w-1/2 px-2">
-                                <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Me</button>
+                                <Link to={`/edit/${blog._id}`}>
+                                    <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Edit Me</button>
+                                </Link>
                             </div>
                             <div className="w-1/2 px-2">
                                 <button className="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600" onClick={deleteBlog}>Delete Me</button>
