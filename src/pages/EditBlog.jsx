6327736debf9052a152a037f2d5e3743a7d40c1d
Ajuu01 <raieajuna1005@gmail.com>
2026-02-19 20:14:25 +0545

    corrected backend url

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -137,35 +137,32 @@ function EditBlog(){
 
        console.log(data)
     return (
-      <>
-       <Navbar />
-       <div class="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
-            <div class="mt-10 text-center font-bold">Wanna edit blog ? </div>
-            <div class="mt-3 text-center text-4xl font-bold">Edit Blog</div>
-<form  onSubmit={editBlog}>
-
-<div class="p-8">
-                <div class="flex gap-4">
-                <input type="text" name="title" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}  />
-
-                <input type="text" name="subtitle" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}   />
+        <>
+            <Navbar/>
+            <div className="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+                <div className="mt-10 text-center font-bold">Wanna Edit a Blog?</div>
+                <div className="mt-3 text-center text-4xl font-bold">Edit a Blog</div>
+                <form onSubmit={editBlog}>
+                <div className="p-8">
+                    <div className="flex gap-4">
+                        <input type="text" name="title" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}/>
+                        <input type="text" name="subtitle" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}/>
+                    </div>
+                    <div className="my-6 flex gap-4">
+                        <img src={`${data.image}`} width="200" alt="Current"/>
+                        <input type="file" name="image" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange}/>
+                    </div>
+                    <div className="">
+                        <textarea name="description" id="text" cols="30" rows="10" className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"  placeholder="Description *" onChange={handleChange} value={data.description}></textarea>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
+                    </div>
                 </div>
-                <div class="my-6 flex gap-4">
-                <input type="file" name="image" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange} />
-                </div>
-                <div class="">
-                <textarea name="description" id="text" cols="30" rows="10" class="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-black" onChange={handleChange} value={data.description} >Description</textarea>
-                </div>
-                <div class="text-center">
-                <button class="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
-                </div>
-  </div>
-
-</form>
-</div>
-       </>
-
-    )
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default EditBlog
