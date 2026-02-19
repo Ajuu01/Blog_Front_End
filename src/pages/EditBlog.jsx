55011f3e8020ddcee8e5d102a76fb638234259d7
Ajuu01 <raieajuna1005@gmail.com>
2026-02-19 19:50:20 +0545

    corrected backend url

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,82 +1,171 @@
-import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import Navbar from "../components/Navbar";
+// import axios from "axios";
+// import { useNavigate, useParams } from "react-router-dom";
+
+// function EditBlog(){
+//     const {id}=useParams()
+//     const navigate=useNavigate()
+    
+//     const [data,setData]=useState({
+//         title:"",
+//         subtitle:"",
+//         image:"",
+//         description:""
+//     })
+
+//     const handleChange=(e)=>{
+//         const {name,value}=e.target
+//         setData({
+//             ...data,
+//             [name]:name==="image"?e.target.files[0]:value
+//         })
+//     }
+//     useEffect(() => {
+//         const fillBlog=async()=>{
+//                 const response=await axios.get("https://mern-3-0-1.onrender.com/"+id)
+//                 const blog=response.data.data
+
+//                 setData({
+//                     title:blog.title,
+//                     subtitle:blog.subtitle,
+//                     image:blog.image,
+//                     description:blog.description
+//                 })
+//         }
+//         fillBlog()
+//     },[id])
+//     const editBlog=async(e)=>{
+//             e.preventDefault()
+            
+//             const response=await axios.patch("https://mern-3-0-1.onrender.com/blog/"+id,data,{
+//                 headers:{
+//                     "Content-Type":"multipart/form-data"
+//                 }
+//             })
+//             if(response.status==200){
+//                 navigate("/")            
+//             }
+//             else{
+//                 alert("Something Went Wrong!")
+//             }
+//         }
+//     return (
+//         <>
+//             <Navbar/>
+//             <div className="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+//                 <div className="mt-10 text-center font-bold">Wanna Edit a Blog?</div>
+//                 <div className="mt-3 text-center text-4xl font-bold">Edit a Blog</div>
+//                 <form onSubmit={editBlog}>
+//                 <div className="p-8">
+//                     <div className="flex gap-4">
+//                         <input type="text" name="title" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}/>
+//                         <input type="text" name="subtitle" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}/>
+//                     </div>
+//                     <div className="my-6 flex gap-4">
+//                         <img src={`${data.image}`} width="200" alt="Current"/>
+//                         <input type="file" name="image" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange}/>
+//                     </div>
+//                     <div className="">
+//                         <textarea name="description" id="text" cols="30" rows="10" className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"  placeholder="Description *" onChange={handleChange} value={data.description}></textarea>
+//                     </div>
+//                     <div className="text-center">
+//                         <button type="submit" className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
+//                     </div>
+//                 </div>
+//                 </form>
+//             </div>
+//         </>
+//     );
+// }
+
+// export default EditBlog
+
+import { useEffect, useState } from "react"
+import Navbar from "../components/Navbar"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom"
 
 function EditBlog(){
-    const {id}=useParams()
-    const navigate=useNavigate()
-    
-    const [data,setData]=useState({
-        title:"",
-        subtitle:"",
-        image:"",
-        description:""
+    const {id} =  useParams()
+    const navigate = useNavigate()
+    const [data,setData] = useState({
+        title : "", 
+        subtitle : "", 
+        description : "", 
+        image : ""
     })
 
-    const handleChange=(e)=>{
-        const {name,value}=e.target
+    const handleChange = (e)=>{
+        const {name,value} = e.target 
         setData({
-            ...data,
-            [name]:name==="image"?e.target.files[0]:value
+            ...data, 
+            [name] : name === "image"  ? e.target.files[0] : value
         })
     }
-    useEffect(() => {
-        const fillBlog=async()=>{
-                const response=await axios.get("https://mern-3-0-1.onrender.com/"+id)
-                const blog=response.data.data
+    const editBlog = async (e)=>{
+        e.preventDefault()
+     const response =  await axios.patch("https://mern-3-0-1.onrender.com/blog/" + id,data,{
+            headers : {
+                "Content-Type" : "multipart/form-data"
+            }
+        })
+        if(response.status === 200){
+            navigate("/blog/" + id)
+        }else{
+            alert("Something went wrong")
+        }
+    }
 
-                setData({
-                    title:blog.title,
-                    subtitle:blog.subtitle,
-                    image:blog.image,
-                    description:blog.description
-                })
-        }
-        fillBlog()
-    },[id])
-    const editBlog=async(e)=>{
-            e.preventDefault()
-            
-            const response=await axios.patch("https://mern-3-0-1.onrender.com/blog/"+id,data,{
-                headers:{
-                    "Content-Type":"multipart/form-data"
-                }
+
+    const fetchSingleBlog = async ()=>{
+        const response  =   await axios.get("https://mern-3-0-1.onrender.com/blog/" + id)
+        if(response.status === 200){
+            setData({
+                title : response.data.data.title, 
+                description : response.data.data.description, 
+                subtitle : response.data.data.subtitle, 
             })
-            if(response.status==200){
-                navigate("/")            
-            }
-            else{
-                alert("Something Went Wrong!")
-            }
+        }else{  
+            alert("something went wrong")
         }
+       }
+   
+       useEffect(()=>{
+           fetchSingleBlog()
+       },[])
+
+       console.log(data)
     return (
-        <>
-            <Navbar/>
-            <div className="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
-                <div className="mt-10 text-center font-bold">Wanna Edit a Blog?</div>
-                <div className="mt-3 text-center text-4xl font-bold">Edit a Blog</div>
-                <form onSubmit={editBlog}>
-                <div className="p-8">
-                    <div className="flex gap-4">
-                        <input type="text" name="title" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}/>
-                        <input type="text" name="subtitle" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}/>
-                    </div>
-                    <div className="my-6 flex gap-4">
-                        <img src={`${data.image}`} width="200" alt="Current"/>
-                        <input type="file" name="image" className="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange}/>
-                    </div>
-                    <div className="">
-                        <textarea name="description" id="text" cols="30" rows="10" className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300"  placeholder="Description *" onChange={handleChange} value={data.description}></textarea>
-                    </div>
-                    <div className="text-center">
-                        <button type="submit" className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
-                    </div>
+      <>
+       <Navbar />
+       <div class="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+            <div class="mt-10 text-center font-bold">Wanna edit blog ? </div>
+            <div class="mt-3 text-center text-4xl font-bold">Edit Blog</div>
+<form  onSubmit={editBlog}>
+
+<div class="p-8">
+                <div class="flex gap-4">
+                <input type="text" name="title" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}  />
+
+                <input type="text" name="subtitle" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}   />
                 </div>
-                </form>
-            </div>
-        </>
-    );
+                <div class="my-6 flex gap-4">
+                <input type="file" name="image" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange} />
+                </div>
+                <div class="">
+                <textarea name="description" id="text" cols="30" rows="10" class="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-black" onChange={handleChange} value={data.description} >Description</textarea>
+                </div>
+                <div class="text-center">
+                <button class="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
+                </div>
+  </div>
+
+</form>
+</div>
+       </>
+
+    )
 }
 
 export default EditBlog
